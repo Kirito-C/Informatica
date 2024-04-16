994f84ec0a439ad262657c5b1b180a7d71777232
79568187 <simone.rossi0407@gmail.com>
2024-04-16 11:50:36 +0200

    finire l'esercizio

--- C++/strcut_es5/main.cpp
+++ C++/strcut_es5/main.cpp
@@ -1,11 +1,77 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
-struct Viaggio{
+using namespace std;
 
+struct Partecipante{
+    string nome;
+    int numeroPettorina;
+    string cognome;
+    int eta;
+    int sec_tempoDiGara;
+    int min_tempoDiGara;
+    int ore_tempoDiGara;
 };
 
-int main() {
+struct Manifestazione{
+    vector<Partecipante> partecipanti;
+};
 
+void VisualizzaCategoria(Manifestazione mani, int category) {
+    if(category == 0) {
+        //juniores
+        for (int i = 0; i < mani.partecipanti.size(); ++i) {
+            if(mani.partecipanti.at(i).eta < 18) {
+
+            }
+        }
+    } else if(category == 1) {
+        //adulti
+        for (int i = 0; i < mani.partecipanti.size(); ++i) {
+            if(mani.partecipanti.at(i).eta < 18) {
+
+            }
+        }
+    } else if(category == 2) {
+        //senior
+        for (int i = 0; i < mani.partecipanti.size(); ++i) {
+            if(mani.partecipanti.at(i).eta < 18) {
+
+            }
+        }
+    }
+}
+int main() {
+    Manifestazione m;
+    Partecipante p;
+    int n;
+
+    cout << "Quanti partecipanti?" << endl;
+    cin >> n;
+
+    for (int i = 0; i < n; ++i) {
+        cout << "nome?" << endl;
+        getline(cin>>ws, p.nome);
+        cout << "cognome?" << endl;
+        getline(cin>>ws, p.nome);
+        cout << "numero pettorina?" << endl;
+        cin >> p.numeroPettorina;
+        cout << "età?" << endl;
+        cin >> p.eta;
+        cout << "secondi del tempo?" << endl;
+        cin >> p.sec_tempoDiGara;
+        cout << "minuti del tempo?" << endl;
+        cin >> p.min_tempoDiGara;
+        cout << "ore del tempo?" << endl;
+        cin >> p.ore_tempoDiGara;
+    }
+
+    cout << "\tcategoria" << "\t   " << "\teta" << endl;
+    for (int i = 0; i < 3; ++i) {
+        VisualizzaCategoria(m, categoria);
+        cout << endl;
+        categoria++;
+    }
     return 0;
 }
